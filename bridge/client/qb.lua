c52245196035f6c1badb518bf857b621f447eeea
DaemonScripts <51380857+DaemonAlex@users.noreply.github.com>
2026-01-13 23:27:41 -0600

    Refactor vehicle key handling and notifications

--- bridge/client/qb.lua
+++ bridge/client/qb.lua
@@ -1,6 +1,21 @@
-if GetResourceState('qb-core') ~= 'started' then return end
-
 local QBCore = exports['qb-core']:GetCoreObject()
+
+function hasPlyLoaded()
+    return QBCore.Functions.GetPlayerData().job ~= nil
+end
+
+function handleVehicleKeys(vehicle)
+    local plate = GetVehicleNumberPlateText(vehicle)
+    TriggerEvent("vehiclekeys:client:SetOwner", plate)
+end
+
+function DoNotification(text, type)
+    if Config.Notify == 'ox_lib' then
+        lib.notify({ title = 'City Worker', description = text, type = type })
+    else
+        QBCore.Functions.Notify(text, type)
+    end
+end
 
 RegisterNetEvent('QBCore:Client:OnPlayerLoaded', function()
     OnPlayerLoaded()
@@ -9,15 +24,3 @@ end)
 RegisterNetEvent('QBCore:Client:OnPlayerUnload', function()
     OnPlayerUnload()
 end)
-
-function handleVehicleKeys(veh)
-    TriggerEvent('vehiclekeys:client:SetOwner', GetVehicleNumberPlateText(veh))
-end
-
-function hasPlyLoaded()
-    return LocalPlayer.state.isLoggedIn
-end
-
-function DoNotification(text, nType)
-    QBCore.Functions.Notify(text, nType)
-end
